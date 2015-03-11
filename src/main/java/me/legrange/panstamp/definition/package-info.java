ab82a2cb196b5f33a2c6b6ee221f55ed9961e8a0
Gideon le Grange <gideon@legrange.me>
2015-03-11 22:45:36 +0200

    Added package-info to event, definition and xml

---
+++ src/main/java/me/legrange/panstamp/definition/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Device definition objects can be used to define device capabilities. The interfaces in this package 
+ * <p>
+ * Application developers can use these classes to create device definitions programmatically or to load 
+ * device definitions from an other format than XML. 
+ * 
+ * @see me.legrange.panstamp.xml
+ * @since 1.0
+ */package me.legrange.panstamp.definition;
---
+++ src/main/java/me/legrange/panstamp/event/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * This package contains convenience class implementations for the main interfaces from the
+ * me.legrange.panstamp package.
+ * <p>
+  When writing specific event handlers, rather than implementing all
+ * interface methods, developers can extend these classes and only override the method(s) they need to to
+ * implement their functionality.
+ * 
+ * @see me.legrange.panstamp.GatewayListener
+ * @see me.legrange.panstamp.PanStampListener
+ * @see me.legrange.panstamp.RegisterListener
+ * @see me.legrange.panstamp.EndpointListener
+
+ * @since 1.0
+ */package me.legrange.panstamp.event;
