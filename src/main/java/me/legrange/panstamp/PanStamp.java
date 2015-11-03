ed94bc899b716210494e14d7cfde0a5b8eeb8511
Gideon le Grange <gideon@legrange.me>
2015-11-03 21:31:40 +0200

    Kludged parsing of extremely large device ID values in XML

--- src/main/java/me/legrange/panstamp/PanStamp.java
+++ src/main/java/me/legrange/panstamp/PanStamp.java
@@ -467,7 +467,7 @@ public final class PanStamp {
                     }
                 }
                 catch (NetworkException ex) {
-                    Logger.getLogger(PanStamp.class.getName()).log(Level.SEVERE, null, ex);
+                    Logger.getLogger(PanStamp.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
                 }
             }
         };
@@ -542,7 +542,7 @@ public final class PanStamp {
                     try {
                         nw.sendCommandMessage(PanStamp.this, id, val);
                     } catch (ModemException ex) {
-                        Logger.getLogger(PanStamp.class.getName()).log(Level.SEVERE, null, ex);
+                        Logger.getLogger(PanStamp.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
                     } finally {
                         removeListener(this);
                     }
